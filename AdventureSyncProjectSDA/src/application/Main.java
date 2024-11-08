41f8fa64b936e4debd8d8f6fd4a29f3d13ec1769
afsah_areeb <afsahareeb7862@gmail.com>
2024-11-08 23:36:23 +0500

    Added HotelOwnerMenu.fxml

--- AdventureSyncProjectSDA/src/application/Main.java
+++ AdventureSyncProjectSDA/src/application/Main.java
@@ -1,23 +1,30 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.Parent;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-		String connectionString="jdbc:sqlserver://DESKTOP-E85OBQM\\SQLEXPRESS;databaseName=db_crms;integratedSecurity=true;encrypt=false";
-		DatabaseManager dbManager=new DatabaseManager(connectionString);
+		//String connectionString="jdbc:sqlserver://DESKTOP-E85OBQM\\SQLEXPRESS;databaseName=db_crms;integratedSecurity=true;encrypt=false";
+		//DatabaseManager dbManager=new DatabaseManager(connectionString);
 		
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("HotelOwnerMenu.fxml"));
+			Parent root = loader.load();  // This loads the FXML and sets the controller automatically
+
+			// Create the scene and set it
+			Scene scene = new Scene(root);
+	        // Set the scene to the primary stage
+	        primaryStage.setScene(scene);
+	        primaryStage.setTitle("Hotel Owner Menu");
+	        primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
