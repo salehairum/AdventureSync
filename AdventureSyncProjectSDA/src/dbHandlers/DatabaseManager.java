b9a9a750a8f184858a4ad2a2dafe9d583a261f4b
Saleha Irum <117927283+salehairum@users.noreply.github.com>
2024-11-14 22:58:47 +0500

    started db handler

--- AdventureSyncProjectSDA/src/dbHandlers/DatabaseManager.java
+++ AdventureSyncProjectSDA/src/dbHandlers/DatabaseManager.java
@@ -1,4 +1,4 @@
-package application;
+package dbHandlers;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
--- AdventureSyncProjectSDA/src/travelAgencyModels/Car.java
+++ AdventureSyncProjectSDA/src/travelAgencyModels/Car.java
@@ -6,6 +6,24 @@ public class Car extends Vehicle{
 	private float rentalFee; 
 	private float costPerKm; 
 	
+	//constructor
+	//with default rental status false
+	public Car(int id, String brand, String model, int year, String plateNumber, float rentalFee, float costPerKm) {
+		super(id, brand, model, year, plateNumber);
+		this.rentalStatus = false;
+		this.rentalFee = rentalFee;
+		this.costPerKm = costPerKm;
+	}
+	
+	//with specific rental status
+	public Car(int id, String brand, String model, int year, String plateNumber, boolean rentalStatus, float rentalFee, float costPerKm) {
+		super(id, brand, model, year, plateNumber);
+		this.rentalStatus = rentalStatus;
+		this.rentalFee = rentalFee;
+		this.costPerKm = costPerKm;
+	}
+
+	//getters and setters
 	public boolean isRented() {
 		return rentalStatus;
 	}
