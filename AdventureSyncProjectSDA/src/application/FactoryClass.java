ee989b88d1c302df2617fbcf2427b239f01be967
Saleha Irum <117927283+salehairum@users.noreply.github.com>
2024-11-27 09:04:20 +0500

    added factory classs

---
+++ AdventureSyncProjectSDA/src/application/FactoryClass.java
@@ -0,0 +1,51 @@
+package application;
+
+import dbHandlers.BusDBHandler;
+import dbHandlers.DatabaseManager;
+import dbHandlers.HotelDBHandler;
+import dbHandlers.TouristDBHandler;
+import dbHandlers.TravelAgencyDBHandler;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import signupForms.AccountMenuView;
+
+public class FactoryClass {
+	DatabaseManager dbManager;
+	HotelDBHandler hotelDB;
+	TravelAgencyDBHandler travelDB;
+	BusDBHandler busDB;
+	TouristDBHandler touristDB;
+	
+	public FactoryClass(Stage primaryStage) {
+		assignConnections();
+		launchMainMenu(primaryStage);
+	}
+	
+	public void launchMainMenu(Stage primaryStage) {
+		try {
+			AccountMenuView mainMenu = new AccountMenuView();
+      
+			Parent root = mainMenu.getRoot();
+			Scene scene = new Scene(root, 750, 500);
+
+			primaryStage.setScene(scene);
+		    primaryStage.setTitle("Account Menu");
+		    primaryStage.show();
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+	}
+	
+	public void assignConnections() {
+		//saleha: E85OBQM
+		//afsah: MHOGR9K
+		String connectionString="jdbc:sqlserver://DESKTOP-E85OBQM\\SQLEXPRESS;databaseName=sdaDB;integratedSecurity=true;encrypt=false";
+		dbManager=new DatabaseManager(connectionString);
+		
+		hotelDB= new HotelDBHandler(dbManager.getConnection());
+		travelDB = new TravelAgencyDBHandler(dbManager.getConnection());
+		busDB = new BusDBHandler(dbManager.getConnection());
+		touristDB = new TouristDBHandler(dbManager.getConnection());
+	}
+}
